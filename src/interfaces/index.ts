f17c692f6e7e3aa91dd7c9c545093fecd8bf8643
salcrom <seya.28@gmail.com>
2024-08-29 15:03:22 +0200

    forntend React- Traducciones

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,4 @@
 
 export * from './orthography.response'
 export * from './pros-cons.response'
+export * from './translateText.response'
--- src/presentation/components/chat-input-boxes/TextMessageBoxSelect.tsx
+++ src/presentation/components/chat-input-boxes/TextMessageBoxSelect.tsx
@@ -21,6 +21,7 @@ export const TextMessageBoxSelect = ({ onSendMessage, placeholder, disableCorrec
     const handleSendMessage = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         if ( message.trim().length === 0 ) return;
+        if ( selectedOption === '' ) return;
 
         onSendMessage( message, selectedOption )
         setMessage('')
