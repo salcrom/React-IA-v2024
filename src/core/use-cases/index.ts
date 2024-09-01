2f672be7b98c8daa7f01daf1ba539bdda0f04d57
salcrom <seya.28@gmail.com>
2024-09-01 18:45:12 +0200

    Frontend REac- Generar y Editar imágenes terminado

--- src/core/use-cases/index.ts
+++ src/core/use-cases/index.ts
@@ -1,8 +1,10 @@
 
 export * from './audio-to-text.use-case'
+export * from './image-generation.use-case'
+export * from './image-variation.use-case'
 export * from './orthography.use-case'
-export * from './pros-cons.use-case'
-export * from './pros-cons-stream.use-case'
 export * from './pros-cons-stream-generator.use-case'
-export * from './translate-text.use-case'
+export * from './pros-cons-stream.use-case'
+export * from './pros-cons.use-case'
 export * from './text-to-audio.use-case'
+export * from './translate-text.use-case'
